79ed5fddfa0535c784589f6745b535bbe2c6b878
prostfk@gmail.com <prostfk@gmail.com>
2018-06-13 19:21:57 +0300

    set up entity

---
+++ src/main/java/by/prostrmk/controller/AuthController.java
@@ -0,0 +1,32 @@
+package by.prostrmk.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/auth")
+public class AuthController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/auth.jsp").forward(req, resp);
+
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if (password.length() > 3){
+            resp.getWriter().write("ok");
+        }else{
+            resp.getWriter().write("bad");
+        }
+
+    }
+}
